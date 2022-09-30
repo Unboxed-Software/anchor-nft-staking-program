7f7d7e1922b0a88fe59e341740c6229c9d47f69c
James Pacheco <jamesrp13@gmail.com>
2022-09-30 17:15:40 -0600

    Initial commit

---
+++ tests/anchor-nft-staking.ts
@@ -0,0 +1,78 @@
+import * as anchor from "@project-serum/anchor"
+import { Program } from "@project-serum/anchor"
+import { AnchorNftStaking } from "../target/types/anchor_nft_staking"
+import { setupNft } from "./utils/setupNft"
+import { PROGRAM_ID as METADATA_PROGRAM_ID } from "@metaplex-foundation/mpl-token-metadata"
+import { expect } from "chai"
+import { getAccount } from "@solana/spl-token"
+import { findCandyMachinesByPublicKeyFieldOperation } from "@metaplex-foundation/js"
+
+describe("anchor-nft-staking", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
+
+  const program = anchor.workspace.AnchorNftStaking as Program<AnchorNftStaking>
+
+  const wallet = anchor.workspace.AnchorNftStaking.provider.wallet
+
+  let delegatedAuthPda: anchor.web3.PublicKey
+  let stakeStatePda: anchor.web3.PublicKey
+  let nft: any
+  let mintAuth: anchor.web3.PublicKey
+  let mint: anchor.web3.PublicKey
+  let tokenAddress: anchor.web3.PublicKey
+
+  before(async () => {
+    ;({ nft, delegatedAuthPda, stakeStatePda, mint, mintAuth, tokenAddress } =
+      await setupNft(program, wallet.payer))
+  })
+
+  it("Stakes", async () => {
+    // Add your test here.
+    await program.methods
+      .stake()
+      .accounts({
+        nftTokenAccount: nft.tokenAddress,
+        nftMint: nft.mintAddress,
+        nftEdition: nft.masterEditionAddress,
+        metadataProgram: METADATA_PROGRAM_ID,
+      })
+      .rpc()
+
+    const account = await program.account.userStakeInfo.fetch(stakeStatePda)
+    expect(account.stakeState === "Staked")
+  })
+
+  it("Redeems", async () => {
+    await program.methods
+      .redeem()
+      .accounts({
+        nftTokenAccount: nft.tokenAddress,
+        stakeMint: mint,
+        userStakeAta: tokenAddress,
+      })
+      .rpc()
+
+    const account = await program.account.userStakeInfo.fetch(stakeStatePda)
+    expect(account.stakeState === "Unstaked")
+    const tokenAccount = await getAccount(provider.connection, tokenAddress)
+  })
+
+  it("Unstakes", async () => {
+    await program.methods
+      .unstake()
+      .accounts({
+        nftTokenAccount: nft.tokenAddress,
+        nftMint: nft.mintAddress,
+        nftEdition: nft.masterEditionAddress,
+        metadataProgram: METADATA_PROGRAM_ID,
+        stakeMint: mint,
+        userStakeAta: tokenAddress,
+      })
+      .rpc()
+
+    const account = await program.account.userStakeInfo.fetch(stakeStatePda)
+    expect(account.stakeState === "Unstaked")
+  })
+})
---
+++ tests/utils/setupNft.ts
@@ -0,0 +1,60 @@
+import {
+  bundlrStorage,
+  keypairIdentity,
+  Metaplex,
+} from "@metaplex-foundation/js"
+import { createMint, getAssociatedTokenAddress } from "@solana/spl-token"
+import * as anchor from "@project-serum/anchor"
+
+export const setupNft = async (program, payer) => {
+  const metaplex = Metaplex.make(program.provider.connection)
+    .use(keypairIdentity(payer))
+    .use(bundlrStorage())
+
+  const nft = await metaplex
+    .nfts()
+    .create({
+      uri: "",
+      name: "Test nft",
+      sellerFeeBasisPoints: 0,
+    })
+    .run()
+
+  console.log("nft metadata pubkey: ", nft.metadataAddress.toBase58())
+  console.log("nft token address: ", nft.tokenAddress.toBase58())
+  const [delegatedAuthPda] = await anchor.web3.PublicKey.findProgramAddress(
+    [Buffer.from("authority")],
+    program.programId
+  )
+  const [stakeStatePda] = await anchor.web3.PublicKey.findProgramAddress(
+    [payer.publicKey.toBuffer(), nft.tokenAddress.toBuffer()],
+    program.programId
+  )
+
+  console.log("delegated authority pda: ", delegatedAuthPda.toBase58())
+  console.log("stake state pda: ", stakeStatePda.toBase58())
+  const [mintAuth] = await anchor.web3.PublicKey.findProgramAddress(
+    [Buffer.from("mint")],
+    program.programId
+  )
+
+  const mint = await createMint(
+    program.provider.connection,
+    payer,
+    mintAuth,
+    null,
+    2
+  )
+  console.log("Mint pubkey: ", mint.toBase58())
+
+  const tokenAddress = await getAssociatedTokenAddress(mint, payer.publicKey)
+
+  return {
+    nft: nft,
+    delegatedAuthPda: delegatedAuthPda,
+    stakeStatePda: stakeStatePda,
+    mint: mint,
+    mintAuth: mintAuth,
+    tokenAddress: tokenAddress,
+  }
+}
